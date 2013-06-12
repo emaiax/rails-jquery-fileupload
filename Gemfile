c7ad6fe681d6b579de8ea7493167b5572e4d16d7
Eduardo Maia <eduvimaia@gmail.com>
2013-06-11 21:45:54 -0300

    initial import

---
+++ Gemfile
@@ -0,0 +1,25 @@
+ruby "2.0.0"
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+# database
+gem 'sqlite3'
+
+# forms, html, css, javascript
+gem "haml"
+gem "haml-rails"
+gem 'jquery-rails'
+gem "simple_form"
+
+# image
+gem 'carrierwave'
+gem 'rmagick'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass'
+  gem 'jquery-fileupload-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJqueryFileupload::Application.config.secret_token = '681689b98bbd10ea636d998e0094538b8b62a8f6a2c7f1f84d70430c23f5120bf97364f275b1a352ddd3dc46b39b4e80098845ef66cbf5fef5abc9c72c4f6b9b'
